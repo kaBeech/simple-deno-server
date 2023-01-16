6e4a40b27b06916740e53b1894b17c255c11cb53
kaBeech <contact@kaBeech.com>
2023-01-16 14:24:44 -0800

    Add date formatting

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { serve } from "https://deno.land/std/http/server.ts";
+import format from "https://deno.land/x/date_fns/format/index.js";
 
 const url = "http://hn.algolia.com/api/v1/search?query=javascript";
 
@@ -7,7 +8,7 @@ const result = await fetch(url).then((result) => result.json());
 const stories = result.hits.map((hit) => ({
   title: hit.title,
   url: hit.url,
-  createdAt: hit.created_at_i,
+  createdAt: format(new Date(hit.created_at_i * 1000), "yyyy-MM-dd"),
 }));
 
 serve((_req) => new Response(JSON.stringify(stories)), {
