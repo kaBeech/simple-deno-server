5982155b379fdfaaa422f794a526c7ab12ea0041
kaBeech <contact@kaBeech.com>
2023-01-16 15:52:46 -0800

    Add some middleware back in

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,21 @@ const app = new Application();
 
 const router = new Router();
 
+const logging = async (ctx: Context, next: Function) => {
+  console.log(`HTTP ${ctx.request.method} on ${ctx.request.url}`);
+  console.log("Hello Deno!!");
+  console.log("returning a response ...");
+  await next();
+};
+
+app.use(logging);
+
 router.get("/", (ctx) => {
   ctx.response.body = "Hello Deno!";
 }).get("/1", (ctx) => {
-  ctx.response.body = "Hello Deno 1";
+  ctx.response.body = "Hello Deno 1!";
 }).get("/2", (ctx) => {
-  ctx.response.body = "Hello Deno 2";
+  ctx.response.body = "Hello Deno 2!";
 });
 
 app.use(router.routes());
@@ -19,13 +28,13 @@ app.use(router.allowedMethods());
 const routerThree = new Router();
 
 routerThree.get("/3", (ctx) => {
-  ctx.response.body = "Hello Deno 3";
+  ctx.response.body = "Hello Deno 3!";
 });
 
 const routerFour = new Router();
 
 routerFour.get("/4", (ctx) => {
-  ctx.response.body = "Hello Deno 4";
+  ctx.response.body = "Hello Deno 4!";
 });
 
 app.use(routerThree.routes());
@@ -33,21 +42,5 @@ app.use(routerThree.allowedMethods());
 
 app.use(routerFour.routes());
 app.use(routerFour.allowedMethods());
-
-// const logging = async (ctx: Context, next: Function) => {
-//   console.log(`HTTP ${ctx.request.method} on ${ctx.request.url}`);
-//   await next();
-// };
-
-// app.use(logging);
-
-// app.use((ctx) => {
-//   console.log("returning a response ...");
-//   ctx.response.body = "Hello Deno!";
-// });
-
-app.addEventListener("listen", () => {
-  console.log(`Listening on localhost:${port}`);
-});
 
 await app.listen({ port });
