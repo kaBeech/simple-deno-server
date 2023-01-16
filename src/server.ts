9754c020e557b6c020ed50a2ce009134948aff60
kaBeech <contact@kaBeech.com>
2023-01-16 15:39:25 -0800

    Add basic routing

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,50 @@
-import { Application, Context } from "https://deno.land/x/oak/mod.ts";
+import { Application, Context, Router } from "https://deno.land/x/oak/mod.ts";
 
 const port = 8000;
 const app = new Application();
 
-const logging = async (ctx, next) => {
-  console.log(`HTTP ${ctx.request.method} on ${ctx.request.url}`);
-  await next();
-};
+const router = new Router();
 
-app.use(logging);
-
-app.use((ctx) => {
-  console.log("returning a response ...");
-  ctx.response.body = "Hello Deno";
+router.get("/", (ctx) => {
+  ctx.response.body = "Hello Deno!";
+}).get("/1", (ctx) => {
+  ctx.response.body = "Hello Deno 1";
+}).get("/2", (ctx) => {
+  ctx.response.body = "Hello Deno 2";
 });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+const routerThree = new Router();
+
+routerThree.get("/3", (ctx) => {
+  ctx.response.body = "Hello Deno 3";
+});
+
+const routerFour = new Router();
+
+routerFour.get("/4", (ctx) => {
+  ctx.response.body = "Hello Deno 4";
+});
+
+app.use(routerThree.routes());
+app.use(routerThree.allowedMethods());
+
+app.use(routerFour.routes());
+app.use(routerFour.allowedMethods());
+
+// const logging = async (ctx: Context, next: Function) => {
+//   console.log(`HTTP ${ctx.request.method} on ${ctx.request.url}`);
+//   await next();
+// };
+
+// app.use(logging);
+
+// app.use((ctx) => {
+//   console.log("returning a response ...");
+//   ctx.response.body = "Hello Deno!";
+// });
 
 app.addEventListener("listen", () => {
   console.log(`Listening on localhost:${port}`);
