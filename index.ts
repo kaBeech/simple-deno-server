f8c2959a5bc2b75aea176cf2364879bbdb870e2f
kaBeech <contact@kaBeech.com>
2023-01-16 15:08:23 -0800

    Move port to environment variable

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { serve } from "https://deno.land/std/http/server.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 
-import { mapStory } from "./stories.js";
+import { mapStory } from "./stories.ts";
 
 const url = "http://hn.algolia.com/api/v1/search?query=javascript";
 
@@ -9,7 +10,7 @@ const result = await fetch(url).then((result) => result.json());
 const stories = result.hits.map(mapStory);
 
 serve((_req) => new Response(JSON.stringify(stories)), {
-  port: 8000,
+  port: parseInt(config()["PORT"]),
 });
 
 console.log("Hello Deno");
---
+++ stories.ts
@@ -0,0 +1,21 @@
+import format from "https://deno.land/x/date_fns/format/index.js";
+
+interface Story {
+  title: string;
+  url: string;
+  created_at_i: number;
+}
+
+interface FormattedStory {
+  title: string;
+  url: string;
+  createdAt: string;
+}
+
+const mapStory = (story: Story): FormattedStory => ({
+  title: story.title,
+  url: story.url,
+  createdAt: format(new Date(story.created_at_i * 1000), "yyyy-MM-dd", {}),
+});
+
+export { mapStory };
