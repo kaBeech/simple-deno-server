b920564dbda04c720d9c93231d61e62e7adf9107
kaBeech <contact@kaBeech.com>
2023-01-16 13:01:36 -0800

    Add basic fetching functionality

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const url = "http://hn.algolia.com/api/v1/search?query=javascript";
+
 console.log("Hello Deno");
+
+fetch(url)
+  .then((result) => result.json())
+  .then((result) => console.log(result.hits));
