fc3f1b6b729978f23098a8055f911410411b902e
kaBeech <contact@kaBeech.com>
2023-01-16 16:31:02 -0800

    Comment out code in files from vanilla branch

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import { serve } from "https://deno.land/std/http/server.ts";
-import { config } from "https://deno.land/x/dotenv/mod";
+// import { serve } from "https://deno.land/std/http/server.ts";
+// import { config } from "https://deno.land/x/dotenv/mod";
 
-import { mapStory } from "./stories.ts";
+// import { mapStory } from "./stories.ts";
 
-const url = "http://hn.algolia.com/api/v1/search?query=javascript";
+// const url = "http://hn.algolia.com/api/v1/search?query=javascript";
 
-const result = await fetch(url).then((result) => result.json());
+// const result = await fetch(url).then((result) => result.json());
 
-const stories = result.hits.map(mapStory);
+// const stories = result.hits.map(mapStory);
 
-serve((_req) => new Response(JSON.stringify(stories)), {
-  port: parseInt(config()["PORT"]),
-});
+// serve((_req) => new Response(JSON.stringify(stories)), {
+//   port: parseInt(config()["PORT"]),
+// });
 
-console.log("Hello Deno");
+// console.log("Hello Deno");
